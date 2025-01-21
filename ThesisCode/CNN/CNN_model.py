c05708dbb8a4c50c255f8ec8a1a9615c67787e83
Zephenias <kevin.kleinhesseling@web.de>
2025-01-21 15:05:13 +0100

    added a CNN version including a simple form of reccurence

--- ThesisCode/CNN/CNN_model.py
+++ ThesisCode/CNN/CNN_model.py
@@ -8,6 +8,8 @@ from gym_env import RedGymEnv
 import json
 
 
+#torch.use_deterministic_algorithms(True)
+#torch.backends.cudnn.deterministic = True
 configpath = os.path.join(os.path.dirname(__file__), "config.json")
 with open(configpath, 'r') as file:
     config = json.load(file)
@@ -25,16 +27,27 @@ params = config.get("params")
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
-        self.fc1 = nn.Linear(params["datadim"], params["interdim1"])
+        self.conv1 = nn.Conv2d(3, 8, kernel_size= 3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(8, 16, kernel_size= 3, stride=1, padding =1)
+        self.pool = nn.MaxPool2d(kernel_size=2)
+        self.fc1 = nn.Linear(params["flat_size"] + params["recurr"] + params["no_img_size"], params["interdim1"])
         self.fc2 = nn.Linear(params["interdim1"], params["interdim2"])
-        self.fc3 = nn.Linear(params["interdim2"], params["outdim"])
+        self.fc3 = nn.Linear(params["interdim2"], params["outdim"] + params["recurr"])
         
-    def forward(self,x):
-        # x = x.view(x.size(0),-1)
+        self.recurrence = torch.zeros(1,params["recurr"])
+        
+    def forward(self,images, non_images):
+        x = torch.tanh(self.conv1(images))
+        x = self.pool(x)
+        x = torch.tanh(self.conv2(x))
+        x = self.pool(x)
+        x = x.view(x.size(0), -1)
+        x = torch.cat([x, non_images, self.recurrence], dim= 1)
         x = torch.tanh(self.fc1(x))
         x = torch.tanh(self.fc2(x))
         x = self.fc3(x)
-        return x
+        self.recurrence = x[:, -80:]
+        return x[:,:7]
 
 
 class Agent():
@@ -119,21 +132,25 @@ class Agent():
 
         torch.manual_seed(newRn)
         
+        sigma = 1
+        
         self.seed_sequence.append(newRn)
 
-        mutations = [torch.randn(self.interdim1,self.datadim), #weights fc1
-        torch.randn(self.interdim1), #bias fc1
-        torch.randn(self.interdim2, self.interdim1), #weights fc2
-        torch.randn(self.interdim2), #bias fc2
-        torch.randn(self.outdim, self.interdim2), #weights fc3
-        torch.randn(self.outdim) # bias fc3
-        ]
-        self.phenotype.fc1.weight.data += mutations[0]
-        self.phenotype.fc1.bias.data += mutations[1]
-        self.phenotype.fc2.weight.data += mutations[2]
-        self.phenotype.fc2.bias.data += mutations[3]
-        self.phenotype.fc3.weight.data += mutations[4]
-        self.phenotype.fc3.bias.data += mutations[5]
+        for i in range (self.phenotype.conv1.weight.shape[0]):
+            noise = torch.randn_like(self.phenotype.conv1.weight.data[i]) * sigma
+            self.phenotype.conv1.weight.data[i] += noise
+        
+        for i in range (self.phenotype.conv2.weight.shape[0]):
+            noise = torch.randn_like(self.phenotype.conv2.weight.data[i]) * sigma
+            self.phenotype.conv2.weight.data[i] += noise
+
+
+        self.phenotype.fc1.weight.data += torch.randn(self.interdim1,self.datadim) * sigma
+        self.phenotype.fc1.bias.data += torch.randn(self.interdim1) * sigma
+        self.phenotype.fc2.weight.data += torch.randn(self.interdim2, self.interdim1) * sigma
+        self.phenotype.fc2.bias.data += torch.randn(self.interdim2) * sigma
+        self.phenotype.fc3.weight.data += torch.randn(self.outdim, self.interdim2) * sigma
+        self.phenotype.fc3.bias.data += torch.randn(self.outdim) * sigma
 
         self.generation += 1
         
@@ -143,16 +160,16 @@ class Agent():
 
 
 def evaluate(env, model):
-    input = prep_obs(env._get_obs())
+    screens, no_img_input = prep_obs(env._get_obs())
     finished = False
     stepSequence = []
     while not finished:
         with torch.no_grad():
-            output = model.phenotype(input)
+            output = model.phenotype(screens, no_img_input)
             max_value, max_index = torch.max(output, dim=1)
             stepSequence.append(max_index.item())
             input,fitness,finished,_ = env.step(max_index)
-            input = prep_obs(input) #adjusts the observation to the required data format
+            screens, no_img_input = prep_obs(input) #adjusts the observation to the required data format
             
     return fitness, stepSequence , model.seed_sequence
 
@@ -164,19 +181,20 @@ def prep_obs(obs):
         torch.tensor([obs["level"]]), 
         obs["badges"], 
         obs["recent_actions"]], dim=0)
-    screen = obs["screens"].unsqueeze(0)
-    screen_flat = screen.view(screen.size(0), -1)
+    # "screens" has dimensions [72 H,80 W, 3 C] needs adjusting
+    screen = (obs["screens"].permute(2,0,1)).unsqueeze(0)
+    screen = screen.float()
+    #screen_flat = screen.view(screen.size(0), -1)
     map = obs["map"].unsqueeze(0)
     map_flat = map.view(map.size(0), -1)
-    print(noimg.shape)
-    print(map_flat.shape)
 
-    return torch.cat([screen_flat, map_flat, noimg.unsqueeze(0)], dim = 1)
+    return screen, torch.cat([map_flat, noimg.unsqueeze(0)], dim = 1)
 
 def eltism_selection(population):
     highest_index, highest_individual = max(
         enumerate(population), key=lambda x: x[1].fitness
     )
+    print(f'highest index: {highest_index} fitness:{population[highest_index].fitness}, Old elite fitness?: {population[len(population)-1].fitness}')
     return highest_individual
 
 def save(agent):
@@ -184,9 +202,9 @@ def save(agent):
     #makes directory in case it is not there yet
     os.makedirs("sav", exist_ok=True)
     #saves into directory for less clutter
-    torch.save(model.phenotype.state_dict(), f"sav/elite_phenotype_gen{agent.generation}_f{agent.fitness}.pth")
+    torch.save(model.phenotype.state_dict(), f"sav/CNN_elite_phenotype_gen{agent.generation}_f{agent.fitness}.pth")
     agent_state = agent.to_state()
-    with open(f"sav/agent_state_gen{agent.generation}_f{agent.fitness}.json", "w") as file:
+    with open(f"sav/CNN_agent_state_gen{agent.generation}_f{agent.fitness}.json", "w") as file:
         json.dump(agent_state, file)
     return
 
@@ -202,7 +220,12 @@ if __name__ == "__main__":
     for i in range (10):
         init_seeds.append(torch.randint(-69420, 69420, (1,)).item())
     for i in range (10):
-        model = Agent(params["datadim"], params["interdim1"], params["interdim2"], params["outdim"], seed_sequence= [init_seeds[i]])
+        model = Agent(
+                        params["flat_size"] + params["recurr"] + params["no_img_size"],
+                        params["interdim1"],
+                        params["interdim2"],
+                        params["outdim"] + params["recurr"],
+                        seed_sequence= [init_seeds[i]])
         model.genotype = model.init_genotype()
         model.phenotype = model.build_phenotype()
         population.append(model)
@@ -214,16 +237,25 @@ if __name__ == "__main__":
         #for model in population:
         #    print(model.fitness, model.action_sequence)
         elite = eltism_selection(population)
+        elite.phenotype.recurrence = torch.zeros(1,80)
         #checks for significant jumps along the way and saves the new elites so they can be reviewed
         if parent_fitness > 0:
             if (elite.fitness - parent_fitness)/parent_fitness >= significance:
                 save(elite)
         parent_fitness = elite.fitness        
-        torch.save(elite.phenotype.state_dict(), "model_weights_MLP.pth")
+        torch.save(elite.phenotype.state_dict(), "model_weights_CNN.pth")
         new_population = []
         for j in range(params["population_size"] -1):
-            model = Agent(params["datadim"], params["interdim1"], params["interdim2"], params["outdim"], seed_sequence = elite.seed_sequence, generation = i, genotype=elite.genotype)
-            model.mutate(torch.randint(-2**31, 2**31 - 1, (1,)).item(), "model_weights_MLP.pth")
+            model = Agent(
+                        params["flat_size"] + params["recurr"] + params["no_img_size"],
+                        params["interdim1"],
+                        params["interdim2"],
+                        params["outdim"] + params["recurr"],
+                        seed_sequence = elite.seed_sequence,
+                        generation = i,
+                        genotype=elite.genotype
+                        )
+            model.mutate(torch.randint(-2**31, 2**31 - 1, (1,)).item(), "model_weights_CNN.pth")
             new_population.append(model)
         new_population.append(elite)
         population = new_population
---
+++ ThesisCode/CNN/run_CNN.sh
@@ -0,0 +1,1 @@
+conda run --live-stream -n MLPtest python CNN_model.py
